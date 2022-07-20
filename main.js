ac59ada50b3b4503f8b3c853b5fcd4bf36fa29ed
Heri Setiawan <heri15002@mai.unpad.ac.id>
2022-07-20 14:29:57 +0700

    + localstorage

--- main.js
+++ main.js
@@ -20,32 +20,51 @@ let formValidation = () => {
   }
 };
 
-let data = {};
+let data = [];
 
 let acceptData = () => {
-  data["text"] = input.value;
+  data.push({
+    text: input.value,
+  });
+
+  localStorage.setItem("data", JSON.stringify(data));
+
   console.log(data);
   createPost();
 };
 
 let createPost = () => {
-  posts.innerHTML += `
-  <div>
-    <p>${data.text}</p>
-    <span class="options">
-      <i onClick="editPost(this)" class="fas fa-edit"></i>
-      <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
-    </span>
-  </div>
-  `;
+  posts.innerHTML = "";
+  data.map((x,y)=>{
+    return (posts.innerHTML += `
+      <div id=${y}>
+        <p>${x.text}</p>
+        <span class="options">
+          <i onClick="editPost(this)" class="fas fa-edit"></i>
+          <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+        </span>
+      </div>
+      `);
+    })
+
   input.value = "";
 };
 
 let deletePost = (e) => {
   e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
 };
 
 let editPost = (e) => {
   input.value = e.parentElement.previousElementSibling.innerHTML;
-  e.parentElement.parentElement.remove();
+  deletePost(e);
+  localStorage.setItem("data", JSON.stringify(data));
 };
+
+(()=>{
+  data = JSON.parse(localStorage.getItem("data"));
+  createPost();
+  console.log(data);
+})();
